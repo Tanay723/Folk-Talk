403e270131d909f2fb9242ef53b4d56f7676a895
Tanay723 <117100164+Tanay723@users.noreply.github.com>
2025-02-05 04:39:23 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,11 +13,11 @@ const App = () => {
 
    // const user= true; this method can be used for prop drilling , but since we arer using state management tools i.e. zustand in this case, hence we will use below template.
      
-   const{currentUser,isLoading,fetchUserInfo}= useUserStore() // used for state management 
-   const{chatId}= useChatStore()
+   const{currentUser,isLoading,fetchUserInfo}= useUserStore() // used for state management , useUserStore() manages user authentication state.
+   const{chatId}= useChatStore() // useChatStore() handles chat-related state (chatId).
 
-   useEffect(()=>{ // giving error from here but after including auth from ../src/library/firebase , it resolved.
-       const unSub = onAuthStateChanged(auth,(user)=>{
+   useEffect(()=>{ //  Firebase Authentication Handling using useeffect hook
+       const unSub = onAuthStateChanged(auth,(user)=>{  // onAuthStateChanged listens for user authentication status changes
         fetchUserInfo(user?.uid) //it is giviing error in console
         
        })
@@ -29,12 +29,12 @@ const App = () => {
 
     console.log(currentUser)
 
-    if(isLoading) return <div className="loading">Loading...</div>
+    if(isLoading) return <div className="loading">Loading...</div> // If isLoading is true, it shows a loading screen.
 
   return (
     <div className='container'>
       {
-        currentUser ? (
+        currentUser ? ( // If currentUser is authenticated , It Renders the chat list (List), chat window (Chat), and chat details (Detail) when a chat is selected.
         <>
         <List/>
         {chatId && <Chat/>} 
@@ -42,7 +42,7 @@ const App = () => {
 
         </>
                ) 
-        : (<Login/>)
+        : (<Login/>) // Otherwise, it shows the login screen (Login).
       }
 
       <Notification/>
