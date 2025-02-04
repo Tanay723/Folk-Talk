dd9c21ea1cdf6c876cebdb20aee96208dffa91ae
Tanay723 <117100164+Tanay723@users.noreply.github.com>
2025-02-05 04:55:24 +0530

    Update chatstore.js

--- src/library/chatstore.js
+++ src/library/chatstore.js
@@ -1,14 +1,19 @@
+// This code defines a Zustand store for managing chat-related states in a React app
+
 import { doc, getDoc } from 'firebase/firestore';
-import { create } from 'zustand'
+import { create } from 'zustand'  // Store Setup with create()
 import { db } from './firebase';
 import { useUserStore } from './userStore';
 
-export const useChatStore = create((set) => ({
+export const useChatStore = create((set) => ({  // here we are creating chatstore using create 
+
+// state variables
   chatId: null,
   user: null,
   isCurrentUserBlocked: false,
   isReceiverBlocked: false,
 
+  // changeChat method is responsible for switching the current chat and handling blocking logic.
   changeChat: (chatId,user)=>{
       const currentUser = useUserStore.getState().currentUser
        
@@ -18,7 +23,7 @@ export const useChatStore = create((set) => ({
          return set({
           chatId,
           user: null,
-          isCurrentUserBlocked: true,
+          isCurrentUserBlocked: true, // current user is blocked
           isReceiverBlocked: false,
          })
       }
@@ -30,10 +35,12 @@ export const useChatStore = create((set) => ({
         return set({
          chatId,
          user: user,
-         isCurrentUserBlocked: false,
-         isReceiverBlocked: true,
+         isCurrentUserBlocked: false, 
+         isReceiverBlocked: true, // reciever is blocked 
         })
-      }  else
+      } 
+      
+      else // when both are unblocked
 
       {
         return set({
@@ -48,7 +55,7 @@ export const useChatStore = create((set) => ({
   },
 
   changeBlock: ()=>{
-     set((state)=> ({...state,isReceiverBlocked: !state.isReceiverBlocked})) 
+     set((state)=> ({...state,isReceiverBlocked: !state.isReceiverBlocked}))  // It updates the isReceiverBlocked flag, which is a boolean that sees whether user is blocked or unblocekd
   }
   
 }))
